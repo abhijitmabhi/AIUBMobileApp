6ebe53cf374cac6a7b6afc7937f85fc229a949f5
Tanzirul Haque Nayan <tanzirulhaque7@gmail.com>
2019-08-22 12:24:50 +0600

    Courses and Results Details Page Redesigned

--- src/app/pages/student/student-courses-and-results/courses-and-results-details/courses-and-results-details.page.ts
+++ src/app/pages/student/student-courses-and-results/courses-and-results-details/courses-and-results-details.page.ts
@@ -11,50 +11,53 @@ import { CoursesResultsService } from 'src/app/services/student/courses-and-resu
 export class CoursesAndResultsDetailsPage implements OnInit {
 
   //Parameters
-  course:any;
-
-  midHideFlag  = true;
+  course: any;
+  midHideFlag = true;
   finalHideFlag = true;
-
-  midtermResult= [];
-  finaltermResult:any;
-  finalGradeText:any;
-  courseName:any;
-  courseId:any;
-  sectionId:any;
-  facultyNames:any;
-
+  midtermResult = [];
+  finaltermResult: any;
+  finalGradeText: any;
+  courseName: any;
+  courseId: any;
+  sectionId: any;
+  teachers: any;
+  numberOfTeachers: any;
 
   constructor(
-    private coursesResultsService : CoursesResultsService,
+    private coursesResultsService: CoursesResultsService,
     private loadingService: LoadingService,
     private dataService: DataService) { }
 
   ngOnInit() {
     this.onChangeCourse();
+
   }
 
-  onChangeCourse(){
+  onChangeCourse() {
 
     this.loadingService.loadingStart();
 
     this.course = JSON.parse(localStorage.getItem('course'));
     this.course = this.dataService.serviceData;
 
-    this.courseName =this.course.courseName;
+    this.courseName = this.course.courseName;
     this.finalGradeText = this.course.finalTermGrade;
     this.courseId = this.course.courseId;
     this.sectionId = this.course.sectionId;
-    
-    this.coursesResultsService.getCoursesAndResults(this.sectionId, this.courseId).subscribe(res =>{
+
+    this.coursesResultsService.getCoursesAndResults(this.sectionId, this.courseId).subscribe(res => {
       this.midtermResult = res.Data.Exams[0];
       this.finaltermResult = res.Data.Exams[1];
-      this.facultyNames = res.Data.Section.FacaltyNames;
-
+      this.teachers = res.Data.Section.Teachers;
+      this.numberOfTeachers = this.teachers.length;
+      console.log("Number of teachers: "+this.numberOfTeachers);
       console.log(res);
     });
 
     this.loadingService.loadingDismiss();
   }
 
+  isExpanded(numberOfTeachers:any){
+    return numberOfTeachers == 0 || numberOfTeachers == 1 ? true : false;
+  }
 }
